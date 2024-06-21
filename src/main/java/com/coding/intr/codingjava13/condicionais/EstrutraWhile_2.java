0111294b3a1bcb96541937314c62858f240ebc71
relesi <renatolessa.2011@hotmail.com>
2024-06-21 20:22:21 -0300

    add condicional DO While

---
+++ src/main/java/com/coding/intr/codingjava13/condicionais/EstrutraWhile_2.java
@@ -0,0 +1,13 @@
+package com.coding.intr.codingjava13.condicionais;
+
+public class EstrutraWhile_2 {
+
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 20) {
+            System.out.println(++count);
+        }
+
+
+    }
+}
---
+++ src/main/java/com/coding/intr/codingjava13/condicionais/EstruturaDoWhile.java
@@ -0,0 +1,14 @@
+package com.coding.intr.codingjava13.condicionais;
+
+public class EstruturaDoWhile {
+    public static void main(String[] args) {
+//        do {
+//
+//        }while (condicao);
+
+        int i = 0;
+        do {
+            System.out.println(i);
+        }while (i < 20);
+    }
+}
